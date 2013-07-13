6a63e27e196d29a390a36e6e02af02eae46cbf81
Murat Yirci <myirci@gmail.com>
2013-07-13 13:13:11 +0200

    Discription for problem 12 is added.

--- Problem012/problem12.cpp
+++ Problem012/problem12.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
 bool found(unsigned long long  num);
 
-int main() {
+long long barrier1 = numeric_limits<unsigned long long>::max() / 4; 
 
+int main() {
+	
+	cout << "Numeric limits for unsigned long long: " << endl;
+	cout << "Min: " << numeric_limits<unsigned long long>::min() << endl;
+	cout << "Max: " << numeric_limits<unsigned long long>::max() << endl;
+	  
     unsigned long long tri_num = 1;
     unsigned long long i = 1;
 
@@ -20,6 +27,10 @@ int main() {
 }
 
 bool found(unsigned long long num) {
+    
+    if(num >= barrier1) {
+		cout << "Barrier-1 is reached: " << barrier1 << endl;
+	}
     int count = 2;
     for(unsigned long long  i = 2; i < num; ++i) {
         if(num % i == 0) {
@@ -29,5 +40,7 @@ bool found(unsigned long long num) {
     if(count > 500) {
         return true;
     }
+    
+    
     return false;
 }
