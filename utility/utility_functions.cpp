68716bd6f47f591a27ac478b4fd4b0010b4dcaef
Murat Yirci <myirci@gmail.com>
2014-08-28 19:12:12 +0200

    projects are started to organized in a cmake project.

---
+++ utility/utility_functions.cpp
@@ -0,0 +1,13 @@
+
+#include "utility_functions.hpp"
+#include <iostream>
+
+void print_fibonacci_sequence(int n) {
+    if(n < 0) {
+        std::cout << "Fibonacci sequence is defined for postive numbers" << std::endl;
+    }
+
+    for(int i = 0; i <=10; ++i) {
+       // fibonacci(i);
+    }
+}
--- utility/utility_functions.hpp
+++ utility/utility_functions.hpp
@@ -1,13 +1,6 @@
 #ifndef UTILITY_FUNCTIONS_HPP
 #define UTILITY_FUNCTIONS_HPP
 
-// Constraints	: T should be an unsigned integral type. 
-// Functionality: Calculates the sum of all the multiples of a number (n),
-//				  smaller than or equal to the given number (target).
-template <typename T>
-T sum_divisible_by(T target, T n) {
-	T p = target / n;
-	return n*(p*(p+1)/2);
-}
+void print_fibonacci_series(int n);
 
 #endif
