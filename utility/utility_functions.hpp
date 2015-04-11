1d436da4ab2fcdfc49b830fad7e88e2ff85f22f8
Murat Yirci <myirci@gmail.com>
2015-04-12 00:00:14 +0200

    mod.

--- utility/utility_functions.hpp
+++ utility/utility_functions.hpp
@@ -1,19 +1,25 @@
 #ifndef UTILITY_FUNCTIONS_HPP
 #define UTILITY_FUNCTIONS_HPP
 
-// returns the sum of the numbers which are
-//  - divisible by n
-//  - smaller than or equal to upper_limit
+/*
+ * returns the sum of the numbers which are
+ * - divisible by n
+ * - smaller than or equal to upper_limit
+ */
 long long int sum_divisible_by(int n, int upper_limit);
 
-// returns the nth fibonacci number in the fibonacci sequence
-//  - n starts from '0'.
+/*
+ * returns the nth fibonacci number in the fibonacci sequence
+ * - n starts from '0'.
+ */
 long long int fibonacci(int n);
 
-// print min max values of integrals,
-// fundamental integral types: bool, char, char16_t, char32_t, wchar_t, signed char,
-//                             short int, int, long int, long long int, unsigned char,
-//                             unsigned short int, unsigned int, unsigned long int, unsigned long long int
+/*
+ * print min max values of integrals,
+ * fundamental integral types: bool, char, char16_t, char32_t, wchar_t, signed char,
+ * short int, int, long int, long long int, unsigned char, unsigned short int,
+ * unsigned int, unsigned long int, unsigned long long int
+ */
 void print_min_max_values();
 
 
