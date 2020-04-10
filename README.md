8458de6aca4e2b90c1952312ecf4d4ec05a3f718
myirci <myirci@gmail.com>
2020-04-10 21:22:28 +0200

    Problem12 is solved

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Have look at the mathematical problems:
 https://projecteuler.net/
 
 This repository includes some my solutions to some of the Euler project solutions. They are not much but 
-I hope the number of solved problems will gow!
+I hope the number of solved problems will grow!
--- problem_012/problem12.cpp
+++ problem_012/problem12.cpp
@@ -1,46 +1,84 @@
 #include <iostream>
-#include <limits>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
-bool found(unsigned long long  num);
+// Let sDiv be the funtion that returns the set of divisors for a given number
+// Let s1 = sDiv(a) and s2 = sDiv(b)
+// Then, s3 = sDiv(a*b) = unique values of (s1 x s2)
 
-long long barrier1 = numeric_limits<unsigned long long>::max() / 4; 
+// Let tn denote the nth triangle number
+// tn = n*(n+1)/2;
 
-int main() {
-	
-	cout << "Numeric limits for unsigned long long: " << endl;
-	cout << "Min: " << numeric_limits<unsigned long long>::min() << endl;
-	cout << "Max: " << numeric_limits<unsigned long long>::max() << endl;
-	  
-    unsigned long long tri_num = 1;
-    unsigned long long i = 1;
+uint64_t brute_force_count_num_divisors(uint64_t num) {
 
-    do {
-        ++i;
-        tri_num += i;
-    }while(!found(tri_num));
+    if(num == 1)
+    {
+        return 1;
+    }
 
-    cout << "Solution: " << tri_num << endl;
-
-    return 0;
-}
-
-bool found(unsigned long long num) {
-    
-    if(num >= barrier1) {
-		cout << "Barrier-1 is reached: " << barrier1 << endl;
-	}
-    int count = 2;
-    for(unsigned long long  i = 2; i < num; ++i) {
-        if(num % i == 0) {
+    uint64_t count{2};
+    for(uint64_t i = 2; i < num; ++i)
+    {
+        if(num % i == 0)
+        {
             ++count;
         }
     }
-    if(count > 500) {
-        return true;
+    return count;
+}
+
+vector<uint64_t> divisors(uint64_t num)
+{
+    vector<uint64_t> ret{1};
+
+    if(num == 1)
+    {
+        return ret;
     }
-    
-    
-    return false;
+
+    for(uint64_t i = 2; i <= num; ++i)
+    {
+        if(num % i == 0)
+        {
+            ret.push_back(i);
+        }
+    }
+    return ret;
+}
+
+uint64_t num_divisors(const vector<uint64_t>& v1, const vector<uint64_t>& v2)
+{
+    unordered_set<uint64_t> set;
+    for(auto it1 = v1.begin(); it1 != v1.end(); it1++)
+    {
+        for(auto it2 = v2.begin(); it2 != v2.end(); it2++)
+        {
+            set.insert((*it1) * (*it2));
+        }
+    }
+    return set.size();
+}
+
+int main() {
+
+    vector<uint64_t> v1,v2;
+    for(uint64_t i = 2; i < 20000; i++)
+    {
+        auto pairs = i % 2 == 0 ? make_pair(i/2, i+1) : make_pair((i+1)/2, i);
+        v1 = divisors(pairs.first);
+        v2 = divisors(pairs.second);
+        auto num_div = num_divisors(v1,v2);
+        if(num_div > 500)
+        {
+            cout <<"Triangular number: " << i*(i+1)/2 << endl;
+            cout <<"Number of divisors: " << num_div << endl;
+        }
+    }
+
+    cout << "No solution" << endl;
+
+    return 0;
 }
